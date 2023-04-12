8e45471ca67557d0b7c73ed12e89a90fe2cda3e3
marachiii <130127855+marachiii@users.noreply.github.com>
2023-04-12 20:13:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let teks;
+  let day = prompt("How's Your Day??");
+alert("I hope it goes well!")
+alert("Mara punya sesuatu nihh")
+alert("Hope u like it ><")
+
+let nama = prompt("Input Your Name:");
+document.write(`Welcome ${nama}`);
